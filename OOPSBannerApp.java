a8e714c25f26be372ece46ca6fa0f5d01cd5d7af
shauryaprakash1510 <shauryaprakash3315@gmail.com>
2026-03-10 08:25:56 +0530

    UC1: Print OOPS to console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,15 @@
+/**
+ * OOPSBannerApp
+ * UC1: Print OOPS
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("OOPS");
+
+    }
+
+}
+
