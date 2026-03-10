a4dced89a271557637f85f6f5b166ebdc74bce40
shauryaprakash1510 <shauryaprakash3315@gmail.com>
2026-03-10 08:52:40 +0530

    UC6: Refactor banner using static helper methods for character patterns

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,9 +1,9 @@
 /**
  * OOPSBannerApp
- * UC5 - Array Initialization with String.join()
+ * UC6 - Static Helper Methods for Character Patterns
  *
  * @author Shaurya
- * @version 5.0
+ * @version 6.0
  */
 
 public class OOPSBannerApp {
@@ -11,17 +11,56 @@ public class OOPSBannerApp {
     public static void main(String[] args) {
 
         String[] banner = {
-            String.join("", " *****   ", " *****   ", " *****   ", " ***** "),
-            String.join("", "*     *  ", "*     *  ", "*     *  ", "*     *"),
-            String.join("", "*     *  ", "*     *  ", "*     *  ", "*"),
-            String.join("", "*     *  ", "*     *  ", "*     *  ", " ***** "),
-            String.join("", "*     *  ", "*     *  ", "*     *  ", "      *"),
-            String.join("", "*     *  ", "*     *  ", "*     *  ", "*     *"),
-            String.join("", " *****   ", " *****   ", " *****   ", " ***** ")
+            String.join("   ", getO()[0], getO()[0], getP()[0], getS()[0]),
+            String.join("   ", getO()[1], getO()[1], getP()[1], getS()[1]),
+            String.join("   ", getO()[2], getO()[2], getP()[2], getS()[2]),
+            String.join("   ", getO()[3], getO()[3], getP()[3], getS()[3]),
+            String.join("   ", getO()[4], getO()[4], getP()[4], getS()[4]),
+            String.join("   ", getO()[5], getO()[5], getP()[5], getS()[5]),
+            String.join("   ", getO()[6], getO()[6], getP()[6], getS()[6])
         };
 
         for (String line : banner) {
             System.out.println(line);
         }
+    }
+
+    // Pattern for letter O
+    public static String[] getO() {
+        return new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+        };
+    }
+
+    // Pattern for letter P
+    public static String[] getP() {
+        return new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            " ***** ",
+            "*      ",
+            "*      ",
+            "*      "
+        };
+    }
+
+    // Pattern for letter S
+    public static String[] getS() {
+        return new String[]{
+            " ***** ",
+            "*      ",
+            "*      ",
+            " ***** ",
+            "      *",
+            "      *",
+            " ***** "
+        };
     }
 }
