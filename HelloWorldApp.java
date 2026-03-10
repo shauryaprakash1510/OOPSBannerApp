fee3cafbb75ca1c019183eb51d95b920ebbbaa60
shauryaprakash1510 <shauryaprakash3315@gmail.com>
2026-03-10 09:08:00 +0530

    UC2: Display Hello with command line argument

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -1,8 +1,17 @@
+/**
+ * HelloWorldApp
+ * UC2 - Display Hello with command line argument
+ */
+
 public class HelloWorldApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        if (args.length > 0) {
+            System.out.println("Hello " + args[0]);
+        } else {
+            System.out.println("Hello World");
+        }
 
     }
 
