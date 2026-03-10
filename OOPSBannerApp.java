31548cf89e0ca156367467a6b6bf7edc5fa0f358
shauryaprakash1510 <shauryaprakash3315@gmail.com>
2026-03-10 08:41:02 +0530

    UC3: Refactor banner using String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,15 +1,52 @@
 /**
  * OOPSBannerApp
- * UC1: Print OOPS
+ * UC3 - String.join implementation
  */
 
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("OOPS");
+        System.out.println(String.join("",
+                " *****   ",
+                " *****   ",
+                " *****   ",
+                " ***** "));
 
+        System.out.println(String.join("",
+                "*     *  ",
+                "*     *  ",
+                "*     *  ",
+                "*     *"));
+
+        System.out.println(String.join("",
+                "*     *  ",
+                "*     *  ",
+                "*     *  ",
+                "*"));
+
+        System.out.println(String.join("",
+                "*     *  ",
+                "*     *  ",
+                "*     *  ",
+                " ***** "));
+
+        System.out.println(String.join("",
+                "*     *  ",
+                "*     *  ",
+                "*     *  ",
+                "      *"));
+
+        System.out.println(String.join("",
+                "*     *  ",
+                "*     *  ",
+                "*     *  ",
+                "*     *"));
+
+        System.out.println(String.join("",
+                " *****   ",
+                " *****   ",
+                " *****   ",
+                " ***** "));
     }
-
 }
-
