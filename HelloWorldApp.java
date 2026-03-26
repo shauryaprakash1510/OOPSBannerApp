ae5551623cbfe921eb276b91f1cb14d65b2ab374
shauryaprakash1510 <shauryaprakash3315@gmail.com>
2026-03-26 08:58:54 +0530

    UC4: Display Hello for multiple names with default World

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -1,18 +1,14 @@
-/**
- * HelloWorldApp
- * UC2 - Display Hello with command line argument
- */
-
 public class HelloWorldApp {
 
     public static void main(String[] args) {
 
-        if (args.length > 0) {
-            System.out.println("Hello " + args[0]);
-        } else {
+        if (args.length == 0) {
             System.out.println("Hello World");
+        } else {
+            for (String name : args) {
+                System.out.println("Hello " + name);
+            }
         }
 
     }
-
 }
